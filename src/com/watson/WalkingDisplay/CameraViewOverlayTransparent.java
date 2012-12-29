9bd12bd8c358e36347deb83fab271962b5389c05
Jeff Watson <jeff.watson4@gmail.com>
2012-12-29 14:22:59 -0600

    Add License

--- src/com/watson/WalkingDisplay/CameraViewOverlayTransparent.java
+++ src/com/watson/WalkingDisplay/CameraViewOverlayTransparent.java
@@ -1,10 +1,21 @@
-/*
+/**
  * @author Jeff Watson
  * 
- * This file transforms the camera image into a bitmap, which can
- * then have alpha applied, making it transparent. 
- * 
+ *  Copyright 2012 Jeff Watson
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
+
 package com.watson.WalkingDisplay;
 
 import java.io.ByteArrayOutputStream;
